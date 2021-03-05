4c132e2d7375b8e0129a175a6fa4fda5d78014ae
faridEsnaashari <faride13578642@gmail.com>
2021-03-05 14:29:28 +0330

    1- defined shapes state that store all shapes with their detials. 2- defined a callback function that pass to the HierarchyPanel component as 3- an porp. whenever this callback invokes from HierarchyPanel component, shapes state updated. pass shapes state to DrawingPanel component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { useState } from "react";
+
 import "./App.css";
 
 import DrawingPanel from "./components/DrawingPanel/index";
 import HierarchyPanel from "./components/HierarchyPanel/index";
 
 const App = () => {
+    const [shapes, setShapes] = useState([]);
+
+    const updateShapes = shapes => setShapes(shapes);
+
     return(
         <div className="panels-container">
-            <HierarchyPanel></HierarchyPanel>
-            <DrawingPanel></DrawingPanel>
+            <HierarchyPanel onShapesUpdated={ updateShapes }></HierarchyPanel>
+            <DrawingPanel shapes={ shapes }></DrawingPanel>
         </div>
     )
 };
