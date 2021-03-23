e5a0e95d9333bbc6de74bad672150e92e79edb6d
faridEsnaashari <faride13578642@gmail.com>
2021-03-23 19:46:41 +0430

    add /singup route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import SignUpRouter from "./routers/singUp";
 
 import "./App.css";
 
-import DrawingPanel from "./pages/DrawingPanel/index";
-import HierarchyPanel from "./pages/HierarchyPanel/index";
-
 const App = () => {
-    const [shapes, setShapes] = useState([]);
-
-    const updateShapes = shapes => setShapes(shapes);
-
     return(
-        <div className="panels-container">
-            <HierarchyPanel onShapesUpdated={ updateShapes }></HierarchyPanel>
-            <DrawingPanel shapes={ shapes }></DrawingPanel>
-        </div>
-    )
+        <Router>
+            <Switch>
+                <Route path="/signup">
+                    <SignUpRouter/>
+                </Route>
+            </Switch>
+        </Router>
+    );
 };
 export default App;
