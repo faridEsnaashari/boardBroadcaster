ee28f4deefe5601d6fcde1bd335106f1fa1861ff
faridEsnaashari <faride13578642@gmail.com>
2021-03-23 19:47:23 +0430

    add routes informaion for singup routes

---
+++ src/routes/signUp/index.js
@@ -0,0 +1,20 @@
+import SignUpPage from "../../pages/SignUp/SignUpPage";
+import VerificationSentPage from "../../pages/SignUp/VerificationSentPage";
+import VerifyEmailPage from "../../pages/SignUp/VerifyEmailPage";
+
+const routes = [
+    {
+        path: "/verification_sent",
+        component: VerificationSentPage,
+    },
+    {
+        path: "/verify_email",
+        component: VerifyEmailPage,
+    },
+    {
+        path: "",
+        component: SignUpPage,
+    },
+];
+
+export default routes;
