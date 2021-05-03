f8058d162644afba1cd0833cc83af1c24622c128
faridEsnaashari <faride13578642@gmail.com>
2021-05-03 14:55:38 +0430

    baseUrl changed to API_URL. return response in axios response interceptor.

--- src/hooks/useRequestSender.js
+++ src/hooks/useRequestSender.js
@@ -1,18 +1,21 @@
 import axios from "axios";
-import baseUrl from "../tools/config";
+import { API_URL } from "../tools/config";
 import { useState } from "react";
 
 const useRequestSender = () => {
     const [ fetching, setFetching ] = useState(false);
 
-    axios.defaults.baseURL = baseUrl;
+    axios.defaults.baseURL = API_URL;
 
     axios.interceptors.request.use(config => { 
         setFetching(true);
         return config;
     }, () => { setFetching(false) });
 
-    axios.interceptors.response.use(() => setFetching(false));
+    axios.interceptors.response.use((response) => {
+        setFetching(false);
+        return response;
+    });
 
     return { fetching, axios };
 };
