71b3137d85585150a9c8fc8dd4f136cd4a0e70fd
faridEsnaashari <faride13578642@gmail.com>
2021-05-04 15:27:36 +0430

    get verificationToken param from url and send it to verifyEmail route of api

--- src/pages/SignUp/VerifyEmailPage/index.js
+++ src/pages/SignUp/VerifyEmailPage/index.js
@@ -1,6 +1,22 @@
+import { useParams } from "react-router-dom";
+import useRequestSender from "../../../hooks/useRequestSender";
+import { useEffect } from "react";
+
 const VerifyEmailPage = () => {
+    const { verificationToken } = useParams();
+    const { fetching, axios } = useRequestSender();
+
+    useEffect(() => {
+        try{
+            axios.get(`/verifyEmail/${ verificationToken }`);
+        }
+        catch(err){
+            console.log(err);
+        }
+    }, []);
+    
     return (
-        <p>verifyEmailPage</p>
+        <p>{ fetching ? "wait. verifing email." : "email verified." }</p>
     );
 }
 
