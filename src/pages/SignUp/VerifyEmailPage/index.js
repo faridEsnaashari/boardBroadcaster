6001bfc30b7783bcdb8a3280e2eaeb4798920286
faridEsnaashari <faride13578642@gmail.com>
2021-05-06 14:10:19 +0430

    add basic functionality of verify_email page

--- src/pages/SignUp/VerifyEmailPage/index.js
+++ src/pages/SignUp/VerifyEmailPage/index.js
@@ -1,18 +1,41 @@
 import { useParams } from "react-router-dom";
-import useRequestSender from "../../../hooks/useRequestSender";
 import { useEffect } from "react";
+
+import useRequestSender from "../../../hooks/useRequestSender";
+import { INTERNAL_SERVER_ERR, NOTFOUND_ERR, UNAUTHORIZED_ERR, SUCCESS_MSG } from "../../../tools/statusCodes";
 
 const VerifyEmailPage = () => {
     const { verificationToken } = useParams();
     const { fetching, axios } = useRequestSender();
 
-    useEffect(() => {
+    const verifyEmail = async() => {
         try{
-            axios.get(`/verifyEmail/${ verificationToken }`);
+            const result = await axios.get(`/verifyEmail/${ verificationToken }`);
+            if(result.status === SUCCESS_MSG){
+                console.log("email verified successfully");
+            }
         }
         catch(err){
             console.log(err);
+            const { status } = err.response;
+            console.log(status);
+
+            if(status === INTERNAL_SERVER_ERR){
+                console.error("internal server error happened");
+            }
+
+            if(status === NOTFOUND_ERR){
+                console.error("no user found with this email");
+            }
+
+            if(status === UNAUTHORIZED_ERR){
+                console.error("token is invalid");
+            }
         }
+    };
+
+    useEffect(() => {
+        verifyEmail();
     }, []);
     
     return (
