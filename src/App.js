216604270b33acd6e37aeac7bb777ec4a38fd3d4
faridEsnaashari <faride13578642@gmail.com>
2021-06-17 19:33:08 +0430

    Add login route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import SignUpRouter from "./routers/singUp";
+import LogInRouter from "./routers/logIn";
 
 import "./App.css";
 
@@ -8,6 +9,9 @@ const App = () => {
     return(
         <Router>
             <Switch>
+                <Route path="/login">
+                    <LogInRouter/>
+                </Route>
                 <Route path="/signup">
                     <SignUpRouter/>
                 </Route>
