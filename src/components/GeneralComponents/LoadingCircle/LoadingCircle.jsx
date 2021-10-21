b7d4e10215af6e5f4b5459b177a8d921257026d5
faridEsnaashari <faride13578642@gmail.com>
2021-10-21 21:42:09 +0330

    A circle loading component

---
+++ src/components/GeneralComponents/LoadingCircle/LoadingCircle.jsx
@@ -0,0 +1,12 @@
+import "./loadingCircleStyles.css";
+
+const LoadingCircle = props => {
+    return(
+        <div className="loading-circle-main-container">
+            <div className="first-loading-circle"></div>
+            <div className="second-loading-circle"></div>
+        </div>
+    );
+};
+
+export default LoadingCircle;
