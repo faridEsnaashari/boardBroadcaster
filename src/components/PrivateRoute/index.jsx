38450fe8c0378679d2c2de98ad6956df4caca7b4
faridEsnaashari <faride13578642@gmail.com>
2021-10-21 21:45:09 +0330

    A private route that checks user authentication for private routes

---
+++ src/components/PrivateRoute/index.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import { Route, useHistory } from "react-router-dom";
+
+import "./styles.css";
+
+import useAPICaller from "../../APIs/APICallers/APICallers.js";
+
+import UserDetailsContext, { userDetailsContext as userInitialDetailsContext } from "../../contexts/userDetails";
+
+import LoadingCircle from "../GeneralComponents/LoadingCircle/LoadingCircle";
+import Test from "./Test";
+
+const PrivateRoute = props => {
+
+    const { children, path } = props;
+
+    const history = useHistory();
+
+    const [ getUserDetails, userDetails ] = useAPICaller().getUserCaller;
+    useEffect(() => !userDetails.data && !userDetails.isFetching && getUserData(), []);
+
+    const getUserData = () => getUserDetails(true);
+
+    const [ userDetailsContext, setUserDetailsContext ] = useState({ ...userInitialDetailsContext, updateUserDetails: getUserData });
+
+    useEffect(() => !userDetails.isFetching && userDetails.data && setUserDetailsContext({ ...userDetailsContext, user: userDetails.data.user, boards: userDetails.data.boards }), [userDetails]);
+
+    if(userDetails.isFetching){
+        return(
+            <Route path={ path }>
+                <div className="private-route-loading-container">
+                    <div className="private-route-loading-main">
+                        <LoadingCircle/>
+                    </div>
+                </div>
+            </Route>
+        );
+    }
+    if(userDetails.error){
+        history.push("/login");
+        return(
+            <p>Not authorized</p>
+        )
+    }
+    else{
+        return(
+            <Route path={ path }>
+                <UserDetailsContext.Provider value={ userDetailsContext }>
+                    <Test/>
+                </UserDetailsContext.Provider>
+            </Route>
+        );
+    }
+};
+
+export default PrivateRoute;
