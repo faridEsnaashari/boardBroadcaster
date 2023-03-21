e0b5bf4177c2b7eb673c3819403dabf0f6b7361a
faridEsnaashari <faride13578642@gmail.com>
2023-03-21 12:11:30 +0330

    color and thinness of loading circle now are given by props

--- src/components/GeneralComponents/LoadingCircle/LoadingCircle.jsx
+++ src/components/GeneralComponents/LoadingCircle/LoadingCircle.jsx
@@ -1,10 +1,38 @@
 import "./loadingCircleStyles.css";
 
 const LoadingCircle = props => {
+    const {
+        color,
+        size,
+    } = props;
+
+    const getStyle = () => {
+        const style = {};
+
+        if(color){
+            style.borderTopColor = color;
+        }
+
+        if(size){
+            style.borderWidth = size + "px";
+        }
+
+        return style;
+    };
+
     return(
         <div className="loading-circle-main-container">
-            <div className="first-loading-circle"></div>
-            <div className="second-loading-circle"></div>
+            <div 
+                style = { getStyle() }
+                className="first-loading-circle"
+            >
+            </div>
+
+            <div 
+                style = { getStyle() }
+                className="second-loading-circle"
+            >
+            </div>
         </div>
     );
 };
