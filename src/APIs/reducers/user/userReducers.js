b81600eccdc88a00c4cc823a43adc6c4a444086a
faridEsnaashari <faride13578642@gmail.com>
2023-03-25 17:44:00 +0330

    make boards in a specific format when fetched from api

--- src/APIs/reducers/user/userReducers.js
+++ src/APIs/reducers/user/userReducers.js
@@ -6,12 +6,16 @@ import {
     FAILED_USER_GET,
 } from "../../actionsTypes/userActionsTypes";
 
+import { prepareBoard } from "../board/boardCrudReducer";
+
 export const userGetInitialState = {
     isFetching: false,
     status: null,
     data: null,
     error: null,
 };
+
+const prepareBoards = (boards) => boards.map(board => prepareBoard(board));
 
 export const userGetReducer = (state, action) => {
     let currentState = {};
@@ -27,10 +31,15 @@ export const userGetReducer = (state, action) => {
             break;
 
         case RECIVED_USER_GET: 
+            const { boards, ...rest } = action.payload.data;
+
             currentState = {
                 isFetching: false,
                 status: statusCodesDictionary[action.statusCode],
-                data: action.payload.data,
+                data: {
+                    user: rest,
+                    boards: prepareBoards(boards),
+                },
                 error: null,
             };
             break;
