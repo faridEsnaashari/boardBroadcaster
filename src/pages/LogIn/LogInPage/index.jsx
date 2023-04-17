bba60aec18fdf48f9aa4651417494c7c5896c7db
faridEsnaashari <faride13578642@gmail.com>
2023-04-17 18:47:18 +0330

    login ui has been completed

--- src/pages/LogIn/LogInPage/index.jsx
+++ src/pages/LogIn/LogInPage/index.jsx
@@ -7,6 +7,11 @@ import { getElementValue } from "../../../tools/helpers";
 
 import useAPICaller from "../../../APIs/APICallers/APICallers";
 
+import PasswordIcon from "../../../assets/icons/key.png";
+import UserNameIcon from "../../../assets/icons/user.png";
+
+import "./index.css";
+
 const LogInPage = () => {
     const [ login, result ] = useAPICaller().loginCaller;
     const history = useHistory();
@@ -34,17 +39,29 @@ const LogInPage = () => {
     useEffect(() => result.status === SUCCESS_MSG && redirectToBoardsPanel(), [ result.status ]);
 
     return (
-        <>
+        <div className="login-main-container">
+            <div className="login-background-color-1"></div>
+            <div className="login-background-color-2"></div>
+            <div className="login-container">
             <form onSubmit={ logInTheUser }>
-                <label htmlFor="email">email: </label>
-                <input type="text" name="email" id="email"/>
-                <br/>
-                <label htmlFor="password">password: </label>
-                <input type="password" name="password" id="password"/>
-                <br/>
-                <input type="submit"/>
+                <div className="input-container">
+                    <input type="text" name="email" id="email"/>
+                    <div><img src={ UserNameIcon }/></div>
+                </div>
+                <div className="input-container">
+                    <input type="password" name="password" id="password"/>
+                    <div><img src={ PasswordIcon }/></div>
+                </div>
+                <div className="login-submit-main-container">
+                    <div className="login-submit-container">
+                        <div className="hover"></div>
+                        <div></div>
+                        <input type="submit" value="Login"/>
+                    </div>
+                </div>
             </form>
-        </>
+                </div>
+        </div>
     );
 }
 
