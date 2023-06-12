9ebabf2e07ea1114d548f9a9f1644480814cfff4
faridEsnaashari <faride13578642@gmail.com>
2023-06-12 16:33:57 +0330

    two new paths and a redirect path have been added

--- src/routes/app/board/index.js
+++ src/routes/app/board/index.js
@@ -1,9 +1,18 @@
-import BoardPage from "../../../pages/Board/index";
+import ParticipantBoardPage from "../../../pages/Board/Components/ParticipantBoard";
+import PresenterBoardPage from "../../../pages/Board/Components/PresenterBoard";
 
 const routes = [
     {
-        path: "",
-        component: BoardPage,
+        path: "/participant/:id",
+        component: ParticipantBoardPage,
+    },
+    {
+        path: "/presenter/:id",
+        component: PresenterBoardPage,
+    },
+    {
+        type: "redirect",
+        path: "/boards-panel",
     },
 ];
 
